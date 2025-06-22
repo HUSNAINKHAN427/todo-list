79b97784f000d0ca78101aead359c78a13cad4e2
husnain <muhammadhusnainafghan@gmail.com>
2025-06-21 17:19:07 -0700

    add todo files

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
-let input = document.querySelector('#inp')
-let listContainer = document.querySelector('#list-container')
-function addTask() {
-    if (input.value ==='') {
-        alert("write some thing!")
-    }
-    else{
-        let li = document.createElement('li');
-        li.innerHTML = input.value;
-        listContainer.appendChild(li);
-        let span =document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+let input = document.querySelector("#inp");
+let listContainer = document.querySelector("#list-container");
+let addBtn = document.querySelector("#addBtn");
 
-    }
-    input.value ="";
-}
+let editBtn = listContainer.querySelector("#edit")
+//add task
+addBtn.addEventListener("click", () => {
+    if (input.value.trim() === "") return;
+    let li = document.createElement("li");
+    li.innerHTML = `
+             <span class="task-text">${input.value}</span>
+                 <div class="buttons">
+                        <button class="edit">✏️</button>
+                        <button  class="remove">❌</button>
+                </div>`;
+    //remove task
+    listContainer.appendChild(li);
+    let delBtn = li.querySelector(".remove");
+
+    input.value = "";
 
 
-listContainer.addEventListener("click",function(e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked")
-        
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove("")
-    }
-    
-})
+    delBtn.addEventListener("click", () => {
+        li.remove();
+    })
+    // Edit task
+    let editBtn = li.querySelector(".edit");
+    let span = li.querySelector("span");
+    editBtn.addEventListener("click", () => {
+        let newText = prompt("Edit Task ", span.textContent);
+        if (newText !== null && newText.trim() !== "") {
+            span.textContent = newText;
+        }
+    })
+
+
+});
+
